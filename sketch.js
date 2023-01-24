6fee5abd71c41a7437243a025999da04a4e51dc4
gabrielqueiroz08 <100386457+gabrielqueiroz08@users.noreply.github.com>
2023-01-24 18:59:03 -0300

    a

--- sketch.js
+++ sketch.js
@@ -2,43 +2,68 @@ let ground;
 let lander;
 var lander_img;
 var bg_img;
+var bg_inicial;
 
 
 var vx = 0;
 var g = 0.05;
 var vy = 0;
 
-function preload()
-{
+function preload() {
   lander_img = loadImage("normal.png");
   bg_img = loadImage("bg.png");
+  bg_inicial = loadImage("bg_inicial.jpg")
 }
 
 function setup() {
-  createCanvas(1000,700);
+  createCanvas(1000, 700);
   frameRate(80);
 
-  lander = createSprite(100,50,30,30);
+  lander = createSprite(100, 50, 30, 30);
   lander.addImage(lander_img);
   lander.scale = 0.1;
+  lander.setCollider("rectangle", 100, 100, 200, 200)
+
+  ground = createSprite(100, 600, 2800, 20)
 
   rectMode(CENTER);
   textSize(15);
 }
 
-function draw() 
-{
+function draw() {
   background(51);
-  image(bg_img,0,0);
+  image(bg_inicial, 0,0);
+  if(keyCode = "C") {
+    image(bg_img, 0, 0);
+    push()
+    fill(255);
+    text("Velocidade Vertical: " + round(vy), 800, 75);
+    pop();
+  }
+
   push()
   fill(255);
-  text("Velocidade Vertical: "+round(vy),800,75);
+  text("Você é um astronauta em 2154. Você está em uma missão pára colonizar Saturno,\n" + "porém, no caminho sua nave é interceptada por um buraco de minhoca, que leva \n" + "você e sua nave para um universo alternativo" +
+  " : ", 200, 75)
   pop();
 
+  
+
   //descida
-  vy +=g;
-  lander.position.y+=vy;
+  vy += g;
+  lander.position.y += vy;
+
   drawSprites();
 }
 
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    up();
+    lander.changeAnimation("thrusting");
+    thrust.nextFrame();
+  }
+}
 
+function up() {
+  vy = -3
+}
